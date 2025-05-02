d830f30654729b56bb0627f4dd9cdc2d81d5061b
youngjo park <anyways5188@gmail.com>
2025-05-02 14:13:24 +0900

    [refactor] 52p

--- CRA_TDD3/prime-factors.cpp
+++ CRA_TDD3/prime-factors.cpp
@@ -1,10 +1,34 @@
 #include <vector>
 
+using std::vector;
+
 class PrimeFactors {
-	public:
-		std::vector<int> of(int prime)
-		{
-			return { };
+public:
+	std::vector<int> of(int number)
+	{
+		vector<int> result = {};
+		if (number > 1) {
+			int divisor = 2;
+			if (number == 4) {
+				while (number % divisor == 0) {
+					result.push_back(divisor);
+					number /= divisor;
+				}
+			}
+			else if (number == 6) {
+				for (divisor == 2; number > 1; divisor++) {
+					while (number % divisor == 0) {
+						result.push_back(divisor);
+						number /= divisor;
+					}
+				}
+			}
+			else
+			{
+				result.push_back(number);
+			}
 		}
+		return result;
+	}
 
 };
--- CRA_TDD3/prime-factors_test.cpp
+++ CRA_TDD3/prime-factors_test.cpp
@@ -1,11 +1,39 @@
 ﻿#include "gmock/gmock.h"
 #include "prime-factors.cpp"
 
-using std::vector;
-
-TEST(PrimeFactors, Of1)
-{
+class PrimeFixture : public testing::Test {
+public:
 	PrimeFactors prime_factor;
-	std::vector<int> expected = {};
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1)
+{
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1)); // test 테스트
 }
+
+TEST_F(PrimeFixture, Of2)
+{
+	expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2)); // test 테스트
+}
+
+TEST_F(PrimeFixture, Of3)
+{
+	expected = {3};
+	EXPECT_EQ(expected, prime_factor.of(3)); // test 테스트
+}
+
+TEST_F(PrimeFixture, Of4)
+{
+	expected = {2,2};
+	EXPECT_EQ(expected, prime_factor.of(4)); // test 테스트
+} 
+
+TEST_F(PrimeFixture, Of6)
+{
+	expected = {2,3};
+	EXPECT_EQ(expected, prime_factor.of(6)); // test 테스트
+}
+
