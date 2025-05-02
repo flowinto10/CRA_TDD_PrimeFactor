da42bec9a432fbf034189daa9b0b03d845d6e091
youngjo park <anyways5188@gmail.com>
2025-05-02 14:15:24 +0900

    [feature] 54p

--- CRA_TDD3/prime-factors_test.cpp
+++ CRA_TDD3/prime-factors_test.cpp
@@ -37,3 +37,8 @@ TEST_F(PrimeFixture, Of6)
 	EXPECT_EQ(expected, prime_factor.of(6)); // test 테스트
 }
 
+TEST_F(PrimeFixture, Of9)
+{
+	expected = {3,3};
+	EXPECT_EQ(expected, prime_factor.of(9)); // test 테스트
+}
